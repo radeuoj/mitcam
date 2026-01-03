de61476ea526ebd8ba016ed984aef3ca70e2b9cf
Radu <stefan.radu.arghira@gmail.com>
2026-01-03 22:14:36 +0200

    more optimized but there still is a lot to improve

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,31 @@
-use std::collections::hash_map::ValuesMut;
-use std::collections::HashMap;
-use futures_util::{SinkExt, StreamExt, TryStreamExt};
+use axum::body::Bytes;
+use axum::extract::ws::{Message, WebSocket};
+use axum::extract::{ConnectInfo, WebSocketUpgrade};
+use axum::routing::any;
+use axum::{Router, ServiceExt};
+use axum_extra::TypedHeader;
+use futures_util::{SinkExt, StreamExt};
 use nokhwa::pixel_format::RgbAFormat;
 use nokhwa::utils::{ApiBackend, CameraIndex, RequestedFormat, RequestedFormatType};
 use nokhwa::Camera;
+use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::num::NonZeroU32;
 use std::process::exit;
 use std::sync::{Arc, Mutex, RwLock};
 use std::time::Duration;
-use axum::extract::{ConnectInfo, WebSocketUpgrade};
-use axum::extract::ws::{Message, WebSocket};
-use axum::{Router, ServiceExt};
-use axum::body::Bytes;
-use axum::routing::{any, get};
-use axum_extra::TypedHeader;
-use futures_util::stream::SplitSink;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 use tower_http::trace::{DefaultMakeSpan, TraceLayer};
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, EventLoop, OwnedDisplayHandle};
+use winit::event_loop::{ActiveEventLoop, EventLoop, EventLoopProxy, OwnedDisplayHandle};
 use winit::window::{Window, WindowId};
 
 const CAMERA_INDEX: u32 = 1;
+const SLEEP_TIME: Duration = Duration::from_millis(100);
 type Decoder = RgbAFormat;
-type Tx = SplitSink<WebSocket, Message>;
+type Tx = std::sync::mpsc::Sender<Bytes>;
 type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
 
 struct App<'a> {
@@ -36,6 +33,11 @@ struct App<'a> {
     surface: Option<softbuffer::Surface<OwnedDisplayHandle, Arc<Window>>>,
     front_buffer: &'a RwLock<Vec<u8>>,
     size: (u32, u32),
+}
+
+#[derive(Copy, Clone, Debug)]
+enum UserEvent {
+    Redraw,
 }
 
 impl<'a> App<'a> {
@@ -49,11 +51,11 @@ impl<'a> App<'a> {
     }
 }
 
-impl ApplicationHandler for App<'_> {
+impl ApplicationHandler<UserEvent> for App<'_> {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let window_attributes = Window::default_attributes()
             .with_title("Mitcam")
-            .with_inner_size::<winit::dpi::LogicalSize<u32>>(self.size.into())
+            .with_inner_size::<winit::dpi::PhysicalSize<u32>>(self.size.into())
             .with_resizable(false);
         self.window = Some(Arc::new(event_loop.create_window(window_attributes).unwrap()));
 
@@ -63,18 +65,24 @@ impl ApplicationHandler for App<'_> {
         self.surface.as_mut().unwrap().resize(NonZeroU32::new(width).unwrap(), NonZeroU32::new(height).unwrap()).unwrap();
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, event: UserEvent) {
         match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
+            UserEvent::Redraw => self.window.as_ref().unwrap().request_redraw(),
+        }
+    }
+
+    fn window_event(&mut self, _event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => exit(0),
             WindowEvent::RedrawRequested => {
                 let mut buffer = self.surface.as_mut().unwrap().buffer_mut().unwrap();
                 let front_buffer = self.front_buffer.read().unwrap();
+
                 for (screen, chunk) in buffer.iter_mut().zip(front_buffer.as_chunks::<4>().0.iter()) {
                     *screen = (chunk[0] as u32) << 16 | (chunk[1] as u32) << 8 | (chunk[2] as u32);
                 }
 
                 buffer.present().unwrap();
-                self.window.as_mut().unwrap().request_redraw();
             }
             _ => {}
         }
@@ -105,51 +113,88 @@ fn make_buffer(size: (u32, u32)) -> Vec<u8> {
     buffer
 }
 
-fn render(camera: &mut Camera, front_buffer: &RwLock<Vec<u8>>, back_buffer: &mut Vec<u8>) -> Bytes {
-    camera.write_frame_to_buffer::<Decoder>(back_buffer).unwrap();
-    let mut front_buffer = front_buffer.write().unwrap();
-    std::mem::swap(back_buffer, front_buffer.as_mut());
-    Bytes::copy_from_slice(&front_buffer)
-}
-
-fn send_bytes_to_everyone(bytes: Bytes, peer_map: ValuesMut<SocketAddr, Tx>) {
-    unsafe {
-        async_scoped::TokioScope::scope(|s| {
-            for tx in peer_map {
-                s.spawn(tx.send(Message::Binary(bytes.clone())));
-            }
-        });
-    }
-}
-
-async fn run_camera(front_buffer: &RwLock<Vec<u8>>, peer_map: PeerMap) {
+fn run_camera(front_buffer: &RwLock<Vec<u8>>) {
     let mut camera = make_camera();
     camera.open_stream().unwrap();
     let mut back_buffer = make_buffer(get_camera_resolution(&camera));
 
     loop {
-        let bytes = render(&mut camera, &front_buffer, &mut back_buffer);
-        send_bytes_to_everyone(bytes, peer_map.lock().unwrap().values_mut());
-        std::thread::sleep(Duration::from_millis(50));
+        camera.write_frame_to_buffer::<Decoder>(&mut back_buffer).unwrap();
+        let mut front_buffer = front_buffer.write().unwrap();
+        std::mem::swap(&mut back_buffer, &mut front_buffer);
+
+        std::thread::sleep(SLEEP_TIME);
     }
 }
 
-async fn handle_connection(socket: WebSocket, addr: SocketAddr, peer_map: PeerMap) {
-    println!("{addr} connected to websocket");
+async fn run_web_server(peer_map: PeerMap) {
+    let router = Router::new()
+        .route("/ws", any(async move |ws: WebSocketUpgrade, _user_agent: Option<TypedHeader<headers::UserAgent>>, ConnectInfo(addr): ConnectInfo<SocketAddr>| {
+            ws.on_upgrade(move |socket| handle_connection(socket, addr, peer_map))
+        }))
+        .fallback_service(ServeDir::new("web"))
+        .layer(TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::default().include_headers(true)));
 
-    let (tx, mut rx) = socket.split();
+    let listener = TcpListener::bind("0.0.0.0:5555").await.unwrap();
+    println!("Listening on: {}", listener.local_addr().unwrap());
+    axum::serve(listener, router.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
+}
+
+async fn handle_connection(socket: WebSocket, addr: SocketAddr, peer_map: PeerMap) {
+    println!("{addr} connected to the websocket");
+
+    let (mut sender, mut receiver) = socket.split();
+    let (tx, rx) = std::sync::mpsc::channel::<Bytes>();
     peer_map.lock().unwrap().insert(addr, tx);
 
-    while let Some(Ok(msg)) = rx.next().await {
-        match msg {
-            Message::Text(text) => println!("{} sent text: {}", addr, text),
-            Message::Binary(buffer) => println!("{} sent {} bytes", addr, buffer.len()),
-            Message::Close(_) => break,
-            _ => {}
+    let mut recv_task = tokio::task::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            match msg {
+                Message::Close(_) => break,
+                _ => {}
+            }
         }
-    }
+    });
+
+    let mut send_task = tokio::task::spawn(async move {
+        while let Ok(bytes) = rx.recv() {
+            if sender.send(Message::Binary(bytes)).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    tokio::select! {
+        _ = &mut send_task => recv_task.abort(),
+        _ = &mut recv_task => send_task.abort(),
+    };
 
     peer_map.lock().unwrap().remove(&addr);
+    println!("{addr} disconnected from the websocket");
+}
+
+async fn update_native_window(event_loop: EventLoopProxy<UserEvent>) {
+    loop {
+        event_loop.send_event(UserEvent::Redraw).unwrap();
+        tokio::time::sleep(SLEEP_TIME).await;
+    }
+}
+
+async fn update_peers(peer_map: PeerMap, front_buffer: &RwLock<Vec<u8>>) {
+    loop {
+        {
+            let peer_map = peer_map.lock().unwrap();
+            let bytes = Bytes::copy_from_slice(&front_buffer.read().unwrap());
+
+            for tx in peer_map.values() {
+                if tx.send(bytes.clone()).is_err() {
+                    println!("Weird tx");
+                }
+            }
+        }
+
+        tokio::time::sleep(SLEEP_TIME).await;
+    }
 }
 
 #[tokio::main]
@@ -157,50 +202,22 @@ async fn main() {
     print_available_cameras();
     let camera = make_camera();
     let size = get_camera_resolution(&camera);
-    println!("Size: {size:?}");
     let front_buffer = RwLock::new(make_buffer(size));
     let mut app = App::new(size, &front_buffer);
-
-    // tokio::spawn(async move {
-    //     let socket = TcpListener::bind("0.0.0.0:5555").await.unwrap();
-    //     while let Ok((stream, addr)) = socket.accept().await {
-    //         tokio::spawn(handle_connection(stream, addr));
-    //     }
-    // });
-
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
-            }),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-
+    let event_loop = EventLoop::with_user_event().build().unwrap();
     let peer_map = PeerMap::new(Mutex::new(HashMap::new()));
 
     unsafe {
         async_scoped::TokioScope::scope(|s| {
-            s.spawn(async {
-                let peer_map = peer_map.clone();
-                let router = Router::new()
-                    .route("/ws", any(async move |ws: WebSocketUpgrade, _user_agent: Option<TypedHeader<headers::UserAgent>>, ConnectInfo(addr): ConnectInfo<SocketAddr>| {
-                        ws.on_upgrade(move |socket| handle_connection(socket, addr, peer_map))
-                    }))
-                    .fallback_service(ServeDir::new("web"))
-                    .layer(TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::default().include_headers(true)));
+            {
+                let front_buffer = &front_buffer;
+                s.spawn_blocking(move || run_camera(front_buffer));
+            }
 
-                let listener = TcpListener::bind("0.0.0.0:5555").await.unwrap();
-                println!("Listening on: {}", listener.local_addr().unwrap());
-                axum::serve(listener, router.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
-            });
-
-            s.spawn(run_camera(&front_buffer, peer_map.clone()));
-
-            let event_loop = EventLoop::new().unwrap();
+            s.spawn(run_web_server(peer_map.clone()));
+            s.spawn(update_native_window(event_loop.create_proxy()));
+            s.spawn(update_peers(peer_map.clone(), &front_buffer));
             event_loop.run_app(&mut app).unwrap();
-
-            exit(0);
         });
     }
 }
--- web/script.js
+++ web/script.js
@@ -4,21 +4,9 @@ canvas.height = 720;
 const context = canvas.getContext("2d");
 
 const ws = new WebSocket(`ws://${window.location.host}/ws`);
-clear();
+ws.binaryType = "arraybuffer";
 
 ws.addEventListener("message", async (e) => {
-  const imageData = new ImageData(new Uint8ClampedArray(await e.data.arrayBuffer()), 1280, 720);
-  console.log(imageData.data);
+  const imageData = new ImageData(new Uint8ClampedArray(e.data), 1280, 720);
   context.putImageData(imageData, 0, 0);
 })
-
-function clear() {
-  const imageData = context.createImageData(canvas.width, canvas.height);
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    imageData.data[i] = 0;
-    imageData.data[i + 1] = 0;
-    imageData.data[i + 2] = 0;
-    imageData.data[i + 3] = 255;
-  }
-  context.putImageData(imageData, 0, 0);
-}
